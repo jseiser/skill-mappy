c50b905b3d98cf6376a8e306d5c1afaf3d9d4345
Justin <justin.seiser@rackspace.com>
2019-06-20 15:35:37 -0400

    group vars

--- __init__.py
+++ __init__.py
@@ -53,8 +53,7 @@ class MappySkill(Skill):
                 hosts = i["hosts"]
                 group_vars = i["groupvars"]
             host_line = "\n".join(hosts)
-            gvars = {k: v for d in group_vars for k, v in d.items()}
-            return_text = f"*Mappy {deployment}*\n```Name: {name}\nID: {group_id}```\n*Hosts*\n{host_line}\n*Group Vars*\n```{gvars}```"
+            return_text = f"*Mappy {deployment}*\n```Name: {name}\nID: {group_id}```\n*Hosts*\n{host_line}\n*Group Vars*\n```{group_vars}```"
             return return_text
         else:
             return_text = f"*Mappy {deployment}*\n"
