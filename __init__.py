29a69b13b1c9bca78d9fb8e7d3323a04a1f49816
Justin <justin.seiser@rackspace.com>
2019-06-20 15:19:00 -0400

    vars formatting

--- __init__.py
+++ __init__.py
@@ -53,8 +53,7 @@ class MappySkill(Skill):
                 hosts = i["hosts"]
                 group_vars = i["groupvars"]
             host_line = "\n".join(hosts)
-            vars_line = "\n".join(group_vars)
-            return_text = f"*Mappy {deployment}*\n```Name: {name}\nID: {group_id}```\n*Hosts*\n{host_line}\n*Group Vars*\n```{vars_line}```"
+            return_text = f"*Mappy {deployment}*\n```Name: {name}\nID: {group_id}```\n*Hosts*\n{host_line}\n*Group Vars*\n```{group_vars}```"
             return return_text
         else:
             return_text = f"*Mappy {deployment}*\n"
